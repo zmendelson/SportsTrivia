f41347def1c20fa4797b7c80c500b42248c9f908
zmendelson <zmendl4@gmail.com>
2019-12-02 16:11:02 -0500

    writing questions

---
+++ assets/js/app.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    let listOfQuestions = [
+        {
+            question: "Which SEC school has the most NCAA Football championships?",
+            answerList: ["Florida", "Georgia", "Alabama", "LSU"],
+            answer: 3
+        },
+
+        {
+            question: "Which football rivalry has been played the most?",
+            answerList: ["Florida vs. Georgia", "Alabama vs. Auburn", "Auburn vs. Georgia", "Ole Miss vs. Mississippi State"],
+            answer: 3
+        },
+
+        {
+            question: "Where did coach Urban Meyer begin his head coaching career?",
+            answerList: ["Utah", "Bowling Green", "Florida", "Illinois State"],
+            answer: 2
+        },
+
+        {
+            question: "Which SEC school has the SECOND most wins of all time?",
+            answerList: ["Georgia", "Alabama", "Tennessee", "Auburn"],
+            answer: 3
+        },
+
+        {
+            question: "Which SEC school has the least amount of wins of all time?",
+            answerList: ["Mississippi State", "South Carolina", "Kentucky", "Vanderbilt"],
+            answer: 1
+        }
+
+        {
+            question: "Which SEC school holds the record for most consecutive SEC Championship game wins?",
+            answerList: ["Alabama", "Florida", "Georgia", "LSU"],
+            answer: 2
+        }
+
+        {
+            question: "Where did coach Nick Saban begin his head coaching career?",
+            answerList: ["Michigan State", "Toledo", "Miami (OH)", "Kent State"],
+            answer: 2
+        },
+
+        {
+            question: "How many national championships did Urban Meyer win at Florida?",
+            answerList: ["3", "1", "4", "2"],
+            answer: 4
+        },
+
+        {
+            question: ""
+        }
+    ]
+
+
+}
